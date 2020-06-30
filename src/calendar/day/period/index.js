68f60c51dacf77bc98226a556fb6b364e72df29b
Tommy Johnson <tommy@theguestbook.com>
2020-06-29 18:14:51 -0700

    attempt to fix date radius

--- src/calendar/day/period/index.js
+++ src/calendar/day/period/index.js
@@ -133,7 +133,10 @@ class Day extends Component {
 
     if (this.props.marking) {
       containerStyle.push({
-        borderRadius: 17
+        borderTopLeftRadius:17,
+        borderTopRightRadius:17,
+        borderBottomLeftRadius:17,
+        borderBottomRightRadius:17
       });
 
       const flags = this.markingStyle;
